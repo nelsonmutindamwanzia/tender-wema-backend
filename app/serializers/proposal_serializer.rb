4b64bcc9024f2deea9e29fe5c85eb0e61c0fb8b7
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-21 13:07:53 +0300

    add CRUD to proposals controller, update serializer

--- app/serializers/proposal_serializer.rb
+++ app/serializers/proposal_serializer.rb
@@ -1,3 +1,3 @@
 class ProposalSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :kra_pin, :company_license, :bank_statements, :tax_compliance, :council_registration, :proposal_amount, :status, :supplier_id, :tender_id
 end
