b327f7533f485e1db1fe5e1ef99c5d22cafe0af9
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-21 13:04:50 +0300

    add create and destroy methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def create
+        user = Supplier.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
