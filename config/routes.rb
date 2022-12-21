5bc587e6366c868597db89f7b95bcda64bd34417
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-21 13:04:24 +0300

    add /signup, /me, /login and /logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :tenderers
   resources :suppliers
   resources :tenders
+
+  post "/signup", to: "suppliers#create"
+  get "/me", to: "suppliers#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
